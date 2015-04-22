54caf37ba425f9e58d08db9e1ad4b927d4ed8a01
Andrew Krock <ajkrock93@gmail.com>
2015-04-21 20:29:32 -0400

    Added first version of motor PID

--- src/main.c
+++ src/main.c
@@ -38,7 +38,8 @@ int main(void){
         uRef = (phi * K[0]) + (phi_dot * K[1]) + (x * K[2]) + (x_dot * K[3]);
 
         //pass uRef (torque) to pid motor controller
-        motor_update_pid(uRef);
+        motor_update_pid_A(uRef);
+		motor_update_pid_B(uRef);
     }
     return 1;
 }
